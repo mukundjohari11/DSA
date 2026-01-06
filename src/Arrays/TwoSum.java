7d9452487ae1e95c77218429b2111de5eed2804a
Mukund Johari <mukundjohari@gmail.com>
2026-01-06 18:55:59 +0530

    Day1:TwoSum

---
+++ src/Arrays/TwoSum.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TwoSum {
+    static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        int[] result = twoSum(arr, target);
+        System.out.println("Indices: " + Arrays.toString(result));
+    }
+}
